02e6a3399347d43ad3fab2de950ce10c754fe590
BenGardiner123 <102151272@student.swin.edu.au>
2020-04-22 15:19:37 +1000

    Task 1.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Func_Meth_Csharp
+{
+    class Program
+    {
+         public static void welcome()
+        {
+            Console.WriteLine("Welcome!");
+        }
+
+        public static void HaveNice()
+        {
+            Console.WriteLine("Have a nice day!");
+        }
+    
+        public static void Main()
+        {
+            welcome();
+            HaveNice();
+        
+        }
+    }
+}
