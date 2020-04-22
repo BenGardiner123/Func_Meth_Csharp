606adbecb324167f93f57cd75d49d30285332872
BenGardiner123 <102151272@student.swin.edu.au>
2020-04-22 16:04:38 +1000

    task.2 complete

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 namespace Func_Meth_Csharp
 {
     class Program
-    {
+    {/* 
          public static void welcome()
         {
             Console.WriteLine("Welcome!");
@@ -19,6 +19,27 @@ namespace Func_Meth_Csharp
             welcome();
             HaveNice();
         
+        } */
+        
+        
+         public static void welcome(string name)
+        {
+            Console.WriteLine("Welcome friend " + name + "!");
         }
+
+        public static void HaveNice()
+        {
+            Console.WriteLine("Have a nice day!");
+        }
+    
+        public static void Main(string[] args)
+        {
+          string johnName;  
+          Console.Write("Please input a name: ");
+          johnName = Console.ReadLine();  
+            welcome(johnName);
+            HaveNice();
+        }
+
     }
 }
