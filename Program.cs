bda85f472691983ce7edc534db21d88b06c9c614
BenGardiner123 <102151272@student.swin.edu.au>
2020-04-22 16:34:50 +1000

    task 3 complete

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@
 namespace Func_Meth_Csharp
 {
     class Program
-    {/* 
+    {
+        //task 1.
+        /* 
          public static void welcome()
         {
             Console.WriteLine("Welcome!");
@@ -21,8 +23,9 @@ namespace Func_Meth_Csharp
         
         } */
         
-        
-         public static void welcome(string name)
+        //task ---2
+       /*   
+            public static void welcome(string name)
         {
             Console.WriteLine("Welcome friend " + name + "!");
         }
@@ -40,6 +43,27 @@ namespace Func_Meth_Csharp
             welcome(johnName);
             HaveNice();
         }
+        */
+    //task 3 
+
+            public static int Sum(int a, int b)
+            {
+                int total;
+                total = a + b;
+                return total;
+            }
+            public static void Main()
+            {
+                Console.Write("Please enter a number: ");
+                int a = Convert.ToInt16(Console.ReadLine());
+                Console.Write("Please enter a second number: ");
+                int b = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("The sum of the two numbers is: {0}", Sum(a, b));
+
+            }
+
+
+
 
     }
 }
